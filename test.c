c0107fa61c2c0a9eca40b5d60a993430762c95b4
john <johnmarchoeksema@gmail.com>
2025-08-09 21:59:25 -0400

    cleaned up print statements and asserts for how to test certain conditions; much happier with terminal output and with code condition checks now

--- test.c
+++ test.c
@@ -3,6 +3,9 @@
 #include <assert.h>
 #include <string.h>
 
+#define TEST(name, condition) \
+	printf("[%s] Testing %s.\n", (condition) ? "PASS" : "FAIL", name)
+
 void test_create_size_destroy();
 void test_insert();
 void test_delete();
@@ -18,71 +21,55 @@ int main() {
 
 void test_create_size_destroy() {
 	
-	// test create
-	printf("Testing hash_create()...\n");
-
+	// table creation
 	hash_table_t* table = hash_create(16);
-	assert(table != NULL);
+	TEST("hash_create()", table != NULL);
 	
-	printf("✓ hash_create() works\n");
-
-	// test size
-	printf("Testing hash_size()...\n");
-	
+	// table size
 	size_t size = hash_size(table);
-	assert(size == 0);
+	TEST("hash_size()", size == 0);
 	
-	printf("✓ hash_size() works\n");
-
-	// test destroy
-	printf("Testing hash_destroy()...\n");
-	
+	// table destroy
 	hash_destroy(table);
-	
-	printf("✓ hash_destroy() completed without crashing\n");
+	TEST("hash_destroy() doesn't crash", true);	
 }
 
 void test_insert() {
 	
 	// creating table
-	printf("Creating hash_table of capacity 4...\n");
 	hash_table_t* table = hash_create(4);
 	size_t size = hash_size(table);
 	char* key = "1";
 	void* value = "dog";
 	bool insert_success;
-	assert(size == 0);
+	TEST("hash_create() of capacity 4", size == 0);
 
 	// inserting +1
-	printf("Inserting 1...\n");
 	insert_success = hash_insert(table, key, value);
 	size = hash_size(table);
-	assert(size == 1);
-	
+	TEST("hash_insert() 1x", size == 1);
+
 	// inserting +2
-	printf("Inserting 2...\n");
 	key = "2";
 	insert_success = hash_insert(table, key, value);
-	printf("Inserting 3...\n");
+	size = hash_size(table);
+	TEST("hash_insert() 2x", size == 2);
+	
+	// inserting +3
 	key = "3";
 	insert_success = hash_insert(table, key, value);
 	size = hash_size(table);
-	assert(size == 3);
+	TEST("hash_insert() 3x (should trigger resize)", size == 3);
 
-	// inserting +3 (should trigger resize; also more nodes than buckets)
-	printf("Inserting 4...\n");
+	// inserting +4, 5, 6 (should trigger resize; also more nodes than buckets)
 	key = "4";
 	insert_success = hash_insert(table, key, value);
-	printf("Inserting 5...\n");
 	key = "5";
 	insert_success = hash_insert(table, key, value);
-	printf("Inserting 6...\n");
 	key = "6";
 	insert_success = hash_insert(table, key, value);
 	size = hash_size(table);
-	assert(size == 6);
-
-	printf("✓ hash_insert() works\n");
+	TEST("hash_insert() 6x (should trigger second resize)", size == 6);
 
 	(void)insert_success;
 }
@@ -90,7 +77,6 @@ void test_insert() {
 void test_delete() {
 
 	// create table
-	printf("Creating hash_table with 2 keys inserted...\n");
 	hash_table_t* table = hash_create(4);
 	char* key = "1";
 	void* value = "dog";
@@ -101,15 +87,13 @@ void test_delete() {
 	key = "2";
 	insert_success = hash_insert(table, key, value);
 	size_t size = hash_size(table);
-	assert(size == 2);
-	printf("✓ created table of size 2.\n");
+	TEST("hash_create() of capacity 4", size == 2);
 
 	// delete object
-	printf("Deleting key = 1 from table\n");
 	delete_success = hash_delete(table, "1");
 	size = hash_size(table);
+	TEST("hash_delete()", size == 1);
 	assert(size == 1);
-	printf("✓ hash_delete() works\n");
 
 	(void)insert_success;
 	(void)delete_success;
@@ -118,7 +102,6 @@ void test_delete() {
 void test_get() {
 	
 	// create table
-	printf("Creating table of with 2 keys inserted...\n");
 	hash_table_t* table = hash_create(4);
 	char* key = "1";
 	void* value = "cat";
@@ -129,15 +112,10 @@ void test_get() {
 	insert_success = hash_insert(table, key, value);
 	size_t size = hash_size(table);
 	assert(size == 2);
-	printf("✓ created table of size 2.\n");
 
 	// get object
-	printf("Preparing to hash_get()...\n");
 	void* retrieved = hash_get(table, key);
-	assert(strcmp((char*)retrieved, "cat") == 0);
-	//printf(value_gotten);
-	//printf("\n");
-	printf("✓ hash_get() works\n");
+	TEST("hash_get()", strcmp((char*)retrieved, "cat") == 0);
 
 	(void)insert_success;
 }
