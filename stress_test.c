2e67cc302d62f7a3340b6a5ff56168f4e7816d80
john <johnmarchoeksema@gmail.com>
2025-08-15 18:47:49 -0400

    everything mostly working now; outputting times and insertions

--- stress_test.c
+++ stress_test.c
@@ -9,49 +9,38 @@ int main() {
 
 	//int random_num = rand(); // on my system, this number is 2147483647
 
-	size_t breakpoint = 1000;
+	size_t breakpoint = 1000000;
 	size_t size_max = 1000000000;
+	size_t table_size;
 	size_t num;
 	char key[16];
 	int* value;
+	time_t current_time, past_time, delta_time;
 
-	
 	hash_table_t* table = hash_create(16);
+	printf("> RANDOM KEY INSERTION BEGUN\n");
+	current_time = time(NULL);
 
        	for (size_t i = 0; i < size_max; i++) {
-		// get random key and value
+		// check for breakpoint
+		if (i % breakpoint == 0) {
+			table_size = hash_size(table);
+
+			// get current time
+			past_time = current_time;
+			current_time = time(NULL);
+			delta_time = difftime(current_time, past_time);
+			printf("%ld insertions; table size: %ld; current time: %ld s; delta time: %ld s\n", i, table_size, current_time, delta_time);
+		}
+
+		// insert random key and value
 		num = rand();
 		snprintf(key, sizeof(key), "%ld", num);
 		value = malloc(sizeof(int));
 		*value = num;
-		
-		// insert random keys
 		hash_insert(table, key, value);
-
-		// check for breakpoint
-		if (i % breakpoint == 0) {
-			printf("inserted # key %d\n", (int)i);
-		}		
+				
 	}	
-
-	/*
-	time_t start_time, end_time;
-
-	start_time = time(NULL);
-	char* time_string = ctime(&start_time);
-	printf("current time: %s\n", time_string);
-	
-	sleep(4);
-
-	printf("current time: %s\n", time_string);
-
-	end_time = time(NULL);
-	char* time_end = ctime(&end_time);
-	printf("current time: %s\n", time_end);
-
-	(void)start_time;
-	//(void)end_time;\
-	*/
 
 	return 0;
 }
